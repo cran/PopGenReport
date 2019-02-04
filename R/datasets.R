fa413731fd6a45d156fc1b644a48979428d8b9bd
Bernd Gruber <bernd.gruber@canberra.edu.au>
2019-02-04 11:13:23 +0000

    version 3.0.4

--- R/datasets.R
+++ R/datasets.R
@@ -28,47 +28,10 @@
 #' @seealso \code{\link{landgenreport}}, \code{\link{genleastcost}}
 #' @keywords datasets
 #' @examples
-#' 
 #' data(landgen)
 #' summary(landgen)
 "landgen"
 
-#' A simulated genind data set with spatial coordinates
-#' 
-#' This data set is used to demonstrate the use of the
-#' \code{\link{landgenreport}} and \code{\link{genleastcost}} functions. It is
-#' a simple spatial \code{genind} object with 100 individuals in 10 populations
-#' and 20 loci with up to 20 alleles per loci.
-#' 
-#' 
-#' @name landgen
-#' @docType data
-#' @author Bernd Gruber \email{(bernd.gruber@@canberra.edu.au}
-#' @seealso \code{\link{landgenreport}}, \code{\link{genleastcost}}
-#' @keywords datasets
-#' @examples
-#' data(landgen)
-#' summary(landgen)
-"fric.raster"
-
-
-#' A raster file [proportion of treeScrub per cell in Hawke Bay, NZ]
-#'
-#' 
-#' 
-#' @name treeScrub
-#' @format raster file
-#' @docType data
-#' @author Bernd Gruber \email{(bernd.gruber@@canberra.edu.au}
-#' @keywords datasets
-#' @references 
-#' Sarre, S.D., Aitken, N., Adamack, A.T., Macdonald, A.J., Gruber, B. & Cowan, P. (2014). Creating new evolutionary pathways through bioinvasion: The population genetics of brushtail possums in New Zealand. Molecular Ecology, 23, 3419-3433.
-#' @examples
-#' \dontrun{
-#' library(raster)
-#' plot(treeScrub)
-#' }
-"treeScrub"
 
 #' A genlight object created via the read.genetable functions [possum data set from Sarre et al. 2015]
 #'
